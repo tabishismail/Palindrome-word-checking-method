171018ad21cccba0dce9001172bda4ba63ffbfbe
tabishismail <tabish554@gmail.com>
2021-04-16 20:02:29 +0500

    practice of basic java

---
+++ app.js
@@ -0,0 +1,64 @@
+// assignment # 4
+
+// Answer # 1
+
+// var myName= "Tabish" ,age=30, carName="Alto"
+
+// ********************************
+
+// Answer # 2
+
+// 5 Legal variable
+
+// myCar , hello_World, Apple , fruit_42 , age
+
+// 5 Illegal variable
+
+// 7star, more$, class , 3-star, True 
+
+// ********************************
+
+// Answer # 3
+
+// var myHeading = "<h1> Rules for naming JS variables </h1>"
+// var para= "Variable names can only contain, number,$ and _ . For example $my_1stVariable <br> Variables must begin with a letter, $ or _. For example $name, _name or name <br>Variable names are case sensitive <br>Variable names should not be JS keywords"
+
+// document.write (myHeading);
+// document.write (para); 
+
+// assignment # 5
+
+// Answer # 1
+
+// var x=3;
+// var y=5;
+// var z= x+y;
+// document.write (z)
+
+// Answer # 2
+
+// var a=x-y;
+// var b =x*y;
+// var c=x/y;
+// var d=x%y;
+// document.write (d)
+
+// Answer # 3
+
+// var myCar
+// var num=5
+// document.write("Value after variable declaration is:" + myCar)
+// myCar=5
+// document.write("<br>Initial value:" +myCar)
+// document.write("<br>Value after increment is:" + ++myCar)
+// myCar=6+7 
+// document.write("<br>Value after addition is:" + myCar)
+// document.write("<br>Value after decrement is:" + --myCar)
+// b=myCar%3
+// document.write("<br>Value after decrement is:" + b)
+
+// Answer # 4
+
+// var ticket=600
+// document.write ("the cost of buying 5 tickets to a movie is a :" + ticket*5+" PKR")
+

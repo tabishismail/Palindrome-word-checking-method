ce36a690ceab691fee30c0b695d283c2e0c16f7f
tabishismail <tabish554@gmail.com>
2021-04-19 23:39:23 +0500

    chapter 9 to 11 assignment 5 done

--- app.js
+++ app.js
@@ -286,3 +286,57 @@
     
 //     alert("Please refill the fuel in your car")
 // }
+
+// Assignment # 5
+
+// ANswer A = true
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+
+// }
+
+// ANswer B = false
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");}
+
+// ANswer C = 1 is false 2 is true 3 false 4 true
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+// ANswer d = true
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// ANswer e = true is working false not working
+// if (true){
+//     alert("True");
+//     }
+
+// if (false){
+//     alert("False");
+//     }
+
+// ANswer f = true
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }

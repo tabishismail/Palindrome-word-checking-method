44a3701a8df72ddf8791f2b36e4b6a97f4d2dbf6
tabishismail <tabish554@gmail.com>
2021-04-27 23:39:54 +0500

    chapter 12 and 13 assignment 7 done

--- app.js
+++ app.js
@@ -496,18 +496,36 @@
 
 // Assignment # 06
 
-var greeting;
-var hour = 13;
-if (hour < 18) {
-greeting = "Good day";
-document.write(greeting)
-}
-else{
-    greeting = "Good evening";
-    document.write(greeting)
-}
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// document.write(greeting)
+// }
+// else{
+//     greeting = "Good evening";
+//     document.write(greeting)
+// }
 
+// Assignment # 07
 
+// var time = +prompt ("Enter Time in 24 Hour Format (e.g: 0000 to 2359)")
+
+// if(time >=0000 && time < 1200){
+//     alert("Good Morning")
+// }
+// else if(time >=1200 && time < 1700 ){
+//     alert("Good Afternoon")
+// }
+// else if(time >=1700 && time < 2100 ){
+//     alert("Good Evening")
+// }
+// else if(time >=2100 && time < 2359 ){
+//     alert("Good Night")
+// }
+// else{
+//     alert("You Have enter wrong time format ")
+// }
 
 
 

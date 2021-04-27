f156d0a3a39e2daa7fe6590b176064737f818054
tabishismail <tabish554@gmail.com>
2021-04-27 23:32:13 +0500

    Chapter 12 and 13 assignment 6 done

--- app.js
+++ app.js
@@ -493,3 +493,21 @@
 // else{
 //     alert("You have enter wrong Password")
 // }
+
+// Assignment # 06
+
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
+document.write(greeting)
+}
+else{
+    greeting = "Good evening";
+    document.write(greeting)
+}
+
+
+
+
+
